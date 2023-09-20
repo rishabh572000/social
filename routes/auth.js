df6faca006ebe646417ffd2e84c5def8110ecde3
rishabhk <rishabh.kumar@octalsoftware.com>
2023-09-20 13:36:10 +0530

    upsh code

--- routes/auth.js
+++ routes/auth.js
@@ -5,22 +5,35 @@ router.post('/register', async(req, res)=>{
     const newUser =  new User({...req.body})
     try{
         const user = await newUser.save()
-        res.status(200).json(user)
+        const formateData = {
+            status: 200,
+            message:'User registered successfully.',
+            data:user
+        }
+        res.status(200).json(formateData)
     }catch(err){
         console.log(err)
     }
     
 })
 
-router.post('/login', async(req, res)=>{
+router.get('/login', async(req, res)=>{
     try{
         const user = await User.findOne({email: req.body.email})
+
+    
 
         !user && res.status(500).json('user not found')
         const validPassword = req.body.password ===user.password
         !validPassword && res.status(500).json('user not found')
 
-        res.status(200).json(user)
+        const formateData = {
+            status: 200,
+            message:'User login successfully.',
+            data:user
+        }
+
+        res.status(200).json(formateData)
     }
     catch(err){
         res.status(500).json('user not found.')
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const User = require('../models/User')
 
 // update user
-router.put("/:id", async(req, res)=>{
+router.put("/updateUser/:id", async(req, res)=>{
     try{
         
         if(req.params.id || req.user.isAdmin){
@@ -10,7 +10,12 @@ router.put("/:id", async(req, res)=>{
                 $set: req.body
             }, { new: true }
             )
-            res.status(200).json('Account has been updated successfully')
+            const formateData = {
+                status: 200,
+                message:'Account has been updated successfully.',
+                data:user
+            }
+            res.status(200).json(formateData)
         }
         else{
             return res.status(403).json('You can update only your account!')
@@ -20,45 +25,67 @@ router.put("/:id", async(req, res)=>{
     }
 })
 // delete user
-router.delete("/:id", async(req, res)=>{
+router.delete("/deleteUser/:id", async(req, res)=>{
     try{
     
         if(req.params.id || req.user.isAdmin){
              await User.deleteOne({_id:req.params.id})
-            res.status(200).json('Account has been deleted successfully')
+             
+            const formateData = {
+                status: 200,
+                message:'Account has been deleted successfully.',
+            }
+            res.status(200).json(formateData)
         }
         else{
             return res.status(403).json('You can deleted only your account!')
         }
     }catch(err){
         res.send(500).json('something went wrong')
-        console.log('mohan', err)
     }
 })
 // get a user
-router.get("/:id", async(req, res)=>{
+router.get("/viewDetails/:id", async(req, res)=>{
     try{
        const user = await User.findById(req.params.id)
-       res.status(200).json(user)
+       if(user){
+            const formateData = {
+            status: 200,
+            message:'User found successfully.',
+            data:user
+            }
+            res.status(200).json(formateData)
+       }
+       else{
+        const formateData = {
+         status: 200,
+         message:'User not found.',
+         data:user
+        }
+        res.status(200).json(formateData)
+       }
     }catch(err){
-        res.status.send(500).json('something went wrong')
+        res.status(500).json('something went wrong')
     }
 })
 // follow a user 
 router.put("/followUser/:id", async(req, res)=>{
     try{
         if(req.params.id !== req.body.followId){
-            let follow=[], following=[];
-            // console.log(req.params.id)
              await User.findByIdAndUpdate(req.params.id, {
                 $set:{following: req.body.followId}
             }, { new: true }
             )
-            await User.findByIdAndUpdate(req.body.followId, {
+             await User.findByIdAndUpdate(req.body.followId, {
                 $set:{followers: req.params.id}
             }, { new: true }
             )
-            res.status(200).json('Account follow successfully')
+            
+            const formateData = {
+                status: 200,
+                message:'Account follow successfully.',
+            }
+            res.status(200).json(formateData)
         }else{
             res.send(200).json("You can't follow yourself")
         }
